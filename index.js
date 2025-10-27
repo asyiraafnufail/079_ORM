19be9dafb4d95ae9c8c512de1f1ea3c4e3697c1c
asyiraafnufail <asyiraaf4@gmail.com>
2025-10-27 09:53:33 +0700

    update index js method

--- index.js
+++ index.js
@@ -20,3 +20,38 @@ db.sequelize.sync()
     .catch((err) => {
         console.log(err);
     });
+
+app.post('/komik', async (req, res) => {
+    const data = req.body;
+    try {
+        const komik = await db.Komik.create(data);
+        res.status(201).json(komik);
+    } catch (err) {
+        res.status(500).json({ error: 'Failed to create komik' });
+    }
+});
+
+app.get('/komik', async (req, res) => {
+    try {
+        const komiks = await db.Komik.findAll();
+        res.send(komik);
+    } catch (err) {
+        res.send(err);
+    }
+});
+
+app.put('/komik/:id', async (req, res) => {
+    const id = req.params.id;
+    const data = req.body;
+
+    try {
+        const komik = await db.Komik.findByPk(id);
+        if (!komik) {
+            return res.status(404).send({ message: 'Komik not found' });
+        }
+        await komik.update(data);
+        res.send({ message: 'Komik updated successfully', komik });
+    } catch (err) {
+        res.status(500).send(err);
+    }
+});
