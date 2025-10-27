1c858b007f73c5f811c61b612c1691a54677355d
asyiraafnufail <asyiraaf4@gmail.com>
2025-10-27 09:24:30 +0700

    menambahkan listen port

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-'use strict';
+const express = require('express');
+const app = express();
+const port = 3000;
+const db = require('./models');
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: false
+}));
+
+app.listen(port, () => {
+    console.log('Server started on port 3000');
+        })
+
+db.sequelize.sync()
+    .then((result) => {
+        app.listen(port, () => {
+            console.log('Server started');
+        });
+    })
+    .catch((err) => {
+        console.log(err);
+    });
